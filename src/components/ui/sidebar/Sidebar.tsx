8b04ab927434f4c0d13cba19ad5ea6b0a3e8ea14
gavasquez <gustavovasquez15@gmail.com>
2024-11-06 14:53:31 -0500

    Diseño Parte No. 1

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,23 +1,78 @@
 'use client';
-import { IoCloseOutline, IoSearchOutline } from 'react-icons/io5';
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from 'react-icons/io5';
+import Link from 'next/link';
+import { useUIStore } from '@/store';
+import clsx from 'clsx';
 
 export const Sidebar = () => {
+
+  const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
+  const closeMenu = useUIStore((state) => state.closeSideMenu);
+
   return (
     <div>
       {/* Background black */ }
-      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-50" />
+      {
+        isSideMenuOpen && (<div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-50" />)
+      }
+
       {/*  Blur */ }
-      <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm" />
+      {
+        isSideMenuOpen && (<div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm" />)
+      }
+      
+      
       {/* Sidemenu */ }
       <nav
-        /* Todo: efecto slide */
-        className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
-        <IoCloseOutline size={ 40 } className="absolute top-4 right-4 cursor-pointer" onClick={ () => console.log( 'Click' ) } />
+        
+        className={
+          clsx(
+            "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+            {
+              "translate-x-full": !isSideMenuOpen, // esta clase mueve todo el contenido de la pagina a la derecha si esta en falso el isSideMenuOpen
+            }
+          )
+        }>
+        <IoCloseOutline size={ 40 } className="absolute top-4 right-4 cursor-pointer" onClick={ closeMenu} />
         {/* Input search */ }
         <div className="relative mt-14">
-          <IoSearchOutline size={20} className="absolute top-2 left-2" />
+          <IoSearchOutline size={ 20 } className="absolute top-2 left-2" />
           <input type="text" placeholder="Buscar" className="w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500" />
         </div>
+        {/* Menu items */ }
+        <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+          <IoPersonOutline size={ 30 } />
+          <span className="ml-3 text-xl">Perfil</span>
+        </Link>
+        <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+          <IoTicketOutline size={ 30 } />
+          <span className="ml-3 text-xl">Ordenes</span>
+        </Link>
+        <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+          <IoLogInOutline size={ 30 } />
+          <span className="ml-3 text-xl">Ingresar</span>
+        </Link>
+        <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+          <IoLogOutOutline size={ 30 } />
+          <span className="ml-3 text-xl">Salir</span>
+        </Link>
+        {/* Line Separator */ }
+        <div className="w-full h-px bg-gray-400 my-10">
+
+          <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+            <IoShirtOutline size={ 30 } />
+            <span className="ml-3 text-xl">Productos</span>
+          </Link>
+          <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+            <IoTicketOutline size={ 30 } />
+            <span className="ml-3 text-xl">Ordenes</span>
+          </Link>
+          <Link href="/" className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
+            <IoPeopleOutline size={ 30 } />
+            <span className="ml-3 text-xl">Usuarios</span>
+          </Link>
+        </div>
+
       </nav>
     </div>
   );
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,8 +1,14 @@
+'use client';
 import Link from 'next/link';
 import { titleFont } from '@/config/font';
 import { IoCartOutline, IoSearchOutline } from 'react-icons/io5';
+import { useUIStore } from '@/store';
 
 export const TopMenu = () => {
+
+
+  const openMenu = useUIStore( ( state ) => state.openSideMenu );
+
   return (
     <nav className="flex px-5 justify-between items-center w-full">
       {/* Logo */ }
@@ -12,27 +18,27 @@ export const TopMenu = () => {
           <span>| Shop</span>
         </Link>
       </div>
-      {/* Center Menu */}
+      {/* Center Menu */ }
       <div className="hidden sm:block">
         <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-300" href="/category/men">Hombres</Link>
         <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-300" href="/category/women">Mujeres</Link>
         <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-300" href="/category/kids">Niños</Link>
       </div>
 
-      {/* Search, Cart, Menu */}
+      {/* Search, Cart, Menu */ }
       <div className="flex items-center">
         <Link href="/search" className="mx-2">
-          <IoSearchOutline className="w-5 h-5"/>
+          <IoSearchOutline className="w-5 h-5" />
         </Link>
         <Link href="/cart" className="mx-2">
           <div className="relative">
             <span className="absolute text-xs px-1 rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white">
               3
             </span>
-            <IoCartOutline className="w-5 h-5"/>
+            <IoCartOutline className="w-5 h-5" />
           </div>
         </Link>
-        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-300">
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-300" onClick={ openMenu }>
           Menú
         </button>
       </div>
