16f84b0f83a8a4287ca458dabeac1bda7fe20e2e
gavasquez <gustavovasquez15@gmail.com>
2024-11-13 15:03:29 -0500

    Fin de la preparacion de la base de datos

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,81 @@
+import { initialData } from './seed';
+import prisma from "../lib/prisma";
+
+async function main() {
+
+  // 1. Borrar registros previos
+  /* await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ]); */
+
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  // Categories
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map( category => {
+    return {
+      name: category,
+    }
+  });
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+  
+  // Products
+  /* const { images, type, ...product1} = products[0]; */
+
+  /* await prisma.product.create({
+    data: {
+      ...product1,
+      category_id: categoriesMap['shirts'],
+    }
+  }); */
+
+  products.forEach( async product => {
+    const { type, images, ...rest } = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        category_id: categoriesMap[type],
+      }
+    });
+
+    // Images
+    /* images.forEach( async image => {
+      await prisma.productImage.create({
+        data: {
+          url: image,
+          productId: dbProduct.id,
+        }
+      });
+    }); */
+
+    // Otra forma
+    const imagesData = images.map((img) =>({ url: img, productId: dbProduct.id }));
+    await prisma.productImage.createMany({
+      data: imagesData
+    });
+
+  });
+
+}
+
+
+(() => {
+  if(process.env.NODE_ENV === 'production') return;
+  main();
+
+})();
