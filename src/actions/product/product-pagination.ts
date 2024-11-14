6ede82950174be159c8dd3ac21389e84386d2fd1
gavasquez <gustavovasquez15@gmail.com>
2024-11-14 08:44:07 -0500

    Paginacion manual

---
+++ src/actions/product/product-pagination.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({ page = 1, take = 12 }: PaginationOptions) => {
+
+  if(isNaN(Number(page))) page = 1;
+  if(page < 1) page = 1;
+
+
+  console.log((page - 1) * take);
+
+  try {
+    const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          }
+        }
+      }
+    });
+    return {
+      products: products.map(( product) => {
+        return {
+          ...product,
+          images: product.ProductImage.map( ( image ) => image.url ) ,
+        }
+      })
+    };
+  } catch (error) {
+    throw new Error('No se pudo obtener los productos');
+  }
+
+};
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,22 @@
+import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductGrid, Title } from '@/components';
-import { initialData } from '@/seed/seed';
+/* import { initialData } from '@/seed/seed'; */
 
-const products = initialData.products;
+/* const products = initialData.products;
+ */
 
-export default function Home() {
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function Home( { searchParams }: Props ) {
+
+  const page = searchParams.page ? parseInt( searchParams.page ) : 1;
+
+  const { products } = await getPaginatedProductsWithImages( { page } );
+
   return (
     <>
       <Title title="Tienda" subTitle="Todos los productos" className="mb-2" />
